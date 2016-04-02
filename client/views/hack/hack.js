cc2ecfd5a9a673acf3b0e6b15c18a4f400a137e7
aakagi <aakagi@purdue.edu>
2016-04-02 14:02:22 -0700

    massive website overhaul

--- client/views/hack/hack.js
+++ client/views/hack/hack.js
@@ -1,5 +1,24 @@
+Template.hack.onRendered(function() {
+  // Get the page height
+  var elem = $('.field-body')[0];
+  console.log(elem);
+  var elemHeight = window.getComputedStyle(elem, null).height;
+  var pxIndex = elemHeight.indexOf('px');
+  var heightNum = Number(elemHeight.slice(0,pxIndex))
+  Session.set('windowHeight', heightNum + 200);
+})
+
 Template.hack.helpers({
-    projects: function() {
-        return projects;
-    }
+  projects: function() {
+    return projects;
+  },
+  windowHeight: function() {
+    return Session.get('windowHeight');
+  }
+});
+
+Template.hack.events({
+  'click .field-container': function () {
+    Session.set('showHack', false);
+  }
 });
--- client/views/index.js
+++ client/views/index.js
@@ -1,174 +1,195 @@
-Template.index.rendered = function () {
+Template.index.events({
+    'click #hack': function () {
+        Session.set('showHack', true);
+    },
+    'click #life': function () {
+        Session.set('showLife', true);
+    },
+    'click #work': function () {
+        Session.set('showWork', true);
+    }
+});
+
+Template.index.helpers({
+    showHack: function() {
+        console.log("change");
+        return Session.get('showHack');;
+    },
+});
+
+
+// Template.index.rendered = function () {
     
 
-//Scroll with anchor
+// //Scroll with anchor
     
-    // $(function() {
-    //     $('a[href*=#]:not([href=#])').click(function() {
-    //         if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-    //             var target = $(this.hash);
-    //             target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-    //             if (target.length) {
-    //                 $('html,body').animate({
-    //                   scrollTop: target.offset().top
-    //                 }, 500);
-    //                 return false;
-    //             }
-    //         }
-    //     });
-    // });
-// Hack JQuery
-    var hackOpen, lifeOpen, workOpen = false;
-    $("#hackField").hover(
-        function() {
-            $("#hackField").addClass("goldHack");
-            $("#entrepreneur").addClass("goldHack");
-            $(".solidLineImg").addClass("goldHack");
-        },
-        function() {
-            $("#hackField").removeClass("goldHack");
-            $("#entrepreneur").removeClass("goldHack");
-            $(".solidLineImg").removeClass("goldHack");
-        }
-    );
-    $("#hackField").click(
-        function(){
-            $("#hackField").toggleClass("goldHackClick");
-            $("#entrepreneur").toggleClass("goldHackClick");
-            $(".solidLineImg").toggleClass("goldHackClick");
-            $("#workField").toggleClass("goldHackBorderClick");
+//     // $(function() {
+//     //     $('a[href*=#]:not([href=#])').click(function() {
+//     //         if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+//     //             var target = $(this.hash);
+//     //             target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+//     //             if (target.length) {
+//     //                 $('html,body').animate({
+//     //                   scrollTop: target.offset().top
+//     //                 }, 500);
+//     //                 return false;
+//     //             }
+//     //         }
+//     //     });
+//     // });
+// // Hack JQuery
+//     var hackOpen, lifeOpen, workOpen = false;
+//     $("#hackField").hover(
+//         function() {
+//             $("#hackField").addClass("goldHack");
+//             $("#entrepreneur").addClass("goldHack");
+//             $(".solidLineImg").addClass("goldHack");
+//         },
+//         function() {
+//             $("#hackField").removeClass("goldHack");
+//             $("#entrepreneur").removeClass("goldHack");
+//             $(".solidLineImg").removeClass("goldHack");
+//         }
+//     );
+//     $("#hackField").click(
+//         function(){
+//             $("#hackField").toggleClass("goldHackClick");
+//             $("#entrepreneur").toggleClass("goldHackClick");
+//             $(".solidLineImg").toggleClass("goldHackClick");
+//             $("#workField").toggleClass("goldHackBorderClick");
 
-            $("#lifeField").removeClass("greenLifeClick");
-            $("#entrepreneur").removeClass("greenLifeClick");
-            $(".solidLineImg").removeClass("greenLifeClick");
-            $("#hackField").removeClass("greenLifeBorderClick");
-            $("#workField").removeClass("greenLifeBorderClick");
+//             $("#lifeField").removeClass("greenLifeClick");
+//             $("#entrepreneur").removeClass("greenLifeClick");
+//             $(".solidLineImg").removeClass("greenLifeClick");
+//             $("#hackField").removeClass("greenLifeBorderClick");
+//             $("#workField").removeClass("greenLifeBorderClick");
 
-            $("#workField").removeClass("blueWorkClick");
-            $("#entrepreneur").removeClass("blueWorkClick");
-            $(".solidLineImg").removeClass("blueWorkClick");
-            $("#hackField").removeClass("blueWorkBorderClick");
+//             $("#workField").removeClass("blueWorkClick");
+//             $("#entrepreneur").removeClass("blueWorkClick");
+//             $(".solidLineImg").removeClass("blueWorkClick");
+//             $("#hackField").removeClass("blueWorkBorderClick");
 
             
-            if(!hackOpen){
-                $("#hackContent").slideDown(400);
-                hackOpen = true;
-                if (lifeOpen) {
-                    $("#lifeContent").slideUp(400);
-                    lifeOpen = false;
-                }
-                if (workOpen) {
-                    $("#workContent").slideUp(400);
-                    workOpen = false;
-                }
-            }
-            else {
-                $("#hackContent").slideUp(400);
-                hackOpen = false;
-            }
-        }
-    );
+//             if(!hackOpen){
+//                 $("#hackContent").slideDown(400);
+//                 hackOpen = true;
+//                 if (lifeOpen) {
+//                     $("#lifeContent").slideUp(400);
+//                     lifeOpen = false;
+//                 }
+//                 if (workOpen) {
+//                     $("#workContent").slideUp(400);
+//                     workOpen = false;
+//                 }
+//             }
+//             else {
+//                 $("#hackContent").slideUp(400);
+//                 hackOpen = false;
+//             }
+//         }
+//     );
 
-    $("#lifeField").hover(
-        function() {
-            $("#lifeField").addClass("greenLife");
-            $("#entrepreneur").addClass("greenLife");
-            $(".solidLineImg").addClass("greenLife");
-            $("#hackField").addClass("greenLifeBorder");
-            $("#workField").addClass("greenLifeBorder");
-        },
-        function() {
-            $("#lifeField").removeClass("greenLife");
-            $("#entrepreneur").removeClass("greenLife");
-            $(".solidLineImg").removeClass("greenLife");
-            $("#hackField").removeClass("greenLifeBorder");
-            $("#workField").removeClass("greenLifeBorder");
-        }
-    );
-    $("#lifeField").click(
-        function(){
-            $("#lifeField").toggleClass("greenLifeClick");
-            $("#entrepreneur").toggleClass("greenLifeClick");
-            $(".solidLineImg").toggleClass("greenLifeClick");
-            $("#hackField").toggleClass("greenLifeBorderClick");
-            $("#workField").toggleClass("greenLifeBorderClick");
+//     $("#lifeField").hover(
+//         function() {
+//             $("#lifeField").addClass("greenLife");
+//             $("#entrepreneur").addClass("greenLife");
+//             $(".solidLineImg").addClass("greenLife");
+//             $("#hackField").addClass("greenLifeBorder");
+//             $("#workField").addClass("greenLifeBorder");
+//         },
+//         function() {
+//             $("#lifeField").removeClass("greenLife");
+//             $("#entrepreneur").removeClass("greenLife");
+//             $(".solidLineImg").removeClass("greenLife");
+//             $("#hackField").removeClass("greenLifeBorder");
+//             $("#workField").removeClass("greenLifeBorder");
+//         }
+//     );
+//     $("#lifeField").click(
+//         function(){
+//             $("#lifeField").toggleClass("greenLifeClick");
+//             $("#entrepreneur").toggleClass("greenLifeClick");
+//             $(".solidLineImg").toggleClass("greenLifeClick");
+//             $("#hackField").toggleClass("greenLifeBorderClick");
+//             $("#workField").toggleClass("greenLifeBorderClick");
 
-            $("#hackField").removeClass("goldHackClick");
-            $("#entrepreneur").removeClass("goldHackClick");
-            $(".solidLineImg").removeClass("goldHackClick");
-            $("#workField").removeClass("goldHackBorderClick");
+//             $("#hackField").removeClass("goldHackClick");
+//             $("#entrepreneur").removeClass("goldHackClick");
+//             $(".solidLineImg").removeClass("goldHackClick");
+//             $("#workField").removeClass("goldHackBorderClick");
 
-            $("#workField").removeClass("blueWorkClick");
-            $("#entrepreneur").removeClass("blueWorkClick");
-            $(".solidLineImg").removeClass("blueWorkClick");
-            $("#hackField").removeClass("blueWorkBorderClick");
+//             $("#workField").removeClass("blueWorkClick");
+//             $("#entrepreneur").removeClass("blueWorkClick");
+//             $(".solidLineImg").removeClass("blueWorkClick");
+//             $("#hackField").removeClass("blueWorkBorderClick");
 
-            if(!lifeOpen){
-                $("#lifeContent").slideDown(400);
-                lifeOpen = true;
-                if (hackOpen) {
-                    $("#hackContent").slideUp(400);
-                    hackOpen = false;
-                }
-                if (workOpen) {
-                    $("#workContent").slideUp(400);
-                    workOpen = false;
-                }
-            }
-            else {
-                $("#lifeContent").slideUp(400);
-                lifeOpen = false;
-            }
-        }
-    );
+//             if(!lifeOpen){
+//                 $("#lifeContent").slideDown(400);
+//                 lifeOpen = true;
+//                 if (hackOpen) {
+//                     $("#hackContent").slideUp(400);
+//                     hackOpen = false;
+//                 }
+//                 if (workOpen) {
+//                     $("#workContent").slideUp(400);
+//                     workOpen = false;
+//                 }
+//             }
+//             else {
+//                 $("#lifeContent").slideUp(400);
+//                 lifeOpen = false;
+//             }
+//         }
+//     );
 
-    $("#workField").hover(
-        function() {
-            $("#workField").addClass("blueWork");
-            $("#entrepreneur").addClass("blueWork");
-            $(".solidLineImg").addClass("blueWork");
-        },
-        function() {
-            $("#workField").removeClass("blueWork");
-            $("#entrepreneur").removeClass("blueWork");
-            $(".solidLineImg").removeClass("blueWork");
-        }
-    );
-    $("#workField").click(
-        function(){
-            $("#workField").toggleClass("blueWorkClick");
-            $("#entrepreneur").toggleClass("blueWorkClick");
-            $(".solidLineImg").toggleClass("blueWorkClick");
-            $("#hackField").toggleClass("blueWorkBorderClick");
+//     $("#workField").hover(
+//         function() {
+//             $("#workField").addClass("blueWork");
+//             $("#entrepreneur").addClass("blueWork");
+//             $(".solidLineImg").addClass("blueWork");
+//         },
+//         function() {
+//             $("#workField").removeClass("blueWork");
+//             $("#entrepreneur").removeClass("blueWork");
+//             $(".solidLineImg").removeClass("blueWork");
+//         }
+//     );
+//     $("#workField").click(
+//         function(){
+//             $("#workField").toggleClass("blueWorkClick");
+//             $("#entrepreneur").toggleClass("blueWorkClick");
+//             $(".solidLineImg").toggleClass("blueWorkClick");
+//             $("#hackField").toggleClass("blueWorkBorderClick");
 
-            $("#hackField").removeClass("goldHackClick");
-            $("#entrepreneur").removeClass("goldHackClick");
-            $(".solidLineImg").removeClass("goldHackClick");
-            $("#workField").removeClass("goldHackBorderClick");
+//             $("#hackField").removeClass("goldHackClick");
+//             $("#entrepreneur").removeClass("goldHackClick");
+//             $(".solidLineImg").removeClass("goldHackClick");
+//             $("#workField").removeClass("goldHackBorderClick");
 
-            $("#lifeField").removeClass("greenLifeClick");
-            $("#entrepreneur").removeClass("greenLifeClick");
-            $(".solidLineImg").removeClass("greenLifeClick");
-            $("#hackField").removeClass("greenLifeBorderClick");
-            $("#workField").removeClass("greenLifeBorderClick");
+//             $("#lifeField").removeClass("greenLifeClick");
+//             $("#entrepreneur").removeClass("greenLifeClick");
+//             $(".solidLineImg").removeClass("greenLifeClick");
+//             $("#hackField").removeClass("greenLifeBorderClick");
+//             $("#workField").removeClass("greenLifeBorderClick");
 
-            if(!workOpen){
-                $("#workContent").slideDown(400);
-                workOpen = true;
-                if (hackOpen) {
-                    $("#hackContent").slideUp(400);
-                    hackOpen = false;
-                }
-                if (lifeOpen) {
-                    $("#lifeContent").slideUp(400);
-                    lifeOpen = false;
-                }
-            }
-            else {
-                $("#workContent").slideUp(400);
-                workOpen = false;
-            }
-        }
-    );
+//             if(!workOpen){
+//                 $("#workContent").slideDown(400);
+//                 workOpen = true;
+//                 if (hackOpen) {
+//                     $("#hackContent").slideUp(400);
+//                     hackOpen = false;
+//                 }
+//                 if (lifeOpen) {
+//                     $("#lifeContent").slideUp(400);
+//                     lifeOpen = false;
+//                 }
+//             }
+//             else {
+//                 $("#workContent").slideUp(400);
+//                 workOpen = false;
+//             }
+//         }
+//     );
 
-};
+// };
+
